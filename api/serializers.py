63d0407bda72169abd95280dd880b788e0b05e61
Tananau Claudiu <tananauclaudiu21@gmail.com>
2021-05-28 22:34:03 +0300

    second commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-
 from .models import *
 
-class ConversationSerializer(serializers.HyperlinkedModelSerializer):
+class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ('__all__')
 
-class ChatSerializer(serializers.HyperlinkedModelSerializer):
+class ChatSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Chat
 		fields = ('__all__')
--- api/views.py
+++ api/views.py
@@ -9,23 +9,21 @@ from .models import *
 
 class ConversationApiView(APIView):
 	
-	def get(self, request, id, *args, **kwargs):
+	def get(self, request, id):
 		conversations = Conversation.objects.filter(id=id)
-		serializer = ConversationSerializer(conversations, many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
+		chat = Chat.objects.filter(conversationId=id)
+		serializer_conversation = ConversationSerializer(conversations, context={'request': request}, many=True)
+		serializer_chat = ChatSerializer(chat, context={'request': request}, many=True)
+		return Response(serializer_conversation.data + serializer_chat.data, status=status.HTTP_200_OK)
 
 class ChatApiView(APIView):
-	def get(self, request, id, *args, **kwargs):
+	def get(self, request, id):
 		chats = Chat.objects.filter(id=id)
-		serializer = ChatSerializer(chats, many=True)
+		serializer = ChatSerializer(chats, context={'request': request}, many=True)
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
-	def post(self, request, *args, **kwargs):
-		data = {
-			'payload': request.data.get('payload'),
-			'userId': request.user.id
-		}
-		serializer = ChatSerializer(data=data)
+	def post(self, request, format=None):
+		serializer = ChatSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
